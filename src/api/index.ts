344fee0921581d26b6ea9420b61ee37b464d7582
kamazim <kainamazim@gmail.com>
2023-11-29 10:40:50 -0300

    url fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
 const api = axios.create({
-    url: apiUrl,
     timeout: 10000,
     headers: {
         Accept: "application/json",
