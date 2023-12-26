1d7a687f5dd9592b1eb5061a40e78a9fea992596
kainamazim <139380849+kainamazim@users.noreply.github.com>
2023-12-26 11:22:29 +0000

    colorMode on localStorage

--- src/providers/Theme/ThemeProvider.tsx
+++ src/providers/Theme/ThemeProvider.tsx
@@ -4,10 +4,12 @@ import {
     type FC,
     type PropsWithChildren,
     createContext,
+    useEffect,
     useMemo,
     useState,
 } from "react";
 
+import type { PaletteMode } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import {
     ThemeProvider as MuiThemeProvider,
@@ -23,16 +25,22 @@ interface ColorModeContextInterface {
     toggleColorMode: () => void;
 }
 
+const COLOR_MODE_KEY = "color-mode";
+
+const defaultColorMode = typeof window !== 'undefined'
+    ? (localStorage.getItem(COLOR_MODE_KEY) as PaletteMode | null)
+    : "light";
+
 export const ColorModeContext = createContext<ColorModeContextInterface>({
-    colorMode: 'light',
+    colorMode: defaultColorMode ?? "light",
     toggleColorMode: () => {},
 });
 
 const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
     const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
-    const [mode, setMode] = useState<PaletteOptions["mode"]>(
-        prefersDarkMode ? "dark" : "light",
+    const [mode, setMode] = useState<PaletteMode>(
+        defaultColorMode ?? (prefersDarkMode ? "dark" : "light"),
     );
 
     const colorMode = useMemo(
@@ -56,6 +64,10 @@ const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
             }),
         [mode],
     );
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') localStorage.setItem(COLOR_MODE_KEY, mode);
+    }, [mode]);
 
     return (
         <ColorModeContext.Provider value={colorMode}>
