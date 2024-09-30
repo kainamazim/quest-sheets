e66ff6f0b8cc2f2b62685f07934c94de5872d051
kamazim <mazimkaina@gmail.com>
2024-09-30 10:47:06 -0300

    fix: numberfield

--- src/components/@common/form/fields/NumberField.tsx
+++ src/components/@common/form/fields/NumberField.tsx
@@ -5,15 +5,17 @@ import TextField, { type TextFieldProps } from "@mui/material/TextField";
 const NumberField: FC<TextFieldProps> = (props) => {
     return (
         <TextField
-            variant={"standard"}
             type={"number"}
-            inputProps={{
-                inputMode: "numeric",
-                pattern: "[0-9]*",
-                min: 0,
-                max: 99,
+            slotProps={{
+                input: {
+                    inputProps: {
+                        inputMode: "numeric",
+                        pattern: "[0-9]*",
+                        min: 0,
+                        max: 99,
+                    },
+                },
             }}
-            fullWidth
             {...props}
         />
     );
